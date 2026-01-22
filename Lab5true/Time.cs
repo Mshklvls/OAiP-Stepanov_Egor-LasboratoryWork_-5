a44510a8f7bd56b4d72370e118b7ab9043358d4c
Егор Степанов <egorstepanovegor08@gmail.com>
2026-01-22 16:08:48 +0300

    Work prototype

---
+++ Lab5true/Time.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab5true
+{
+    internal class Time
+    {
+        public int hour;
+        public int minute;
+        public void minutes()
+        {
+            minute = minute + hour * 60;
+        }
+    }
+}
