e996ad802533f96bea35a27256150ff001a82d00
Егор Степанов <egorstepanovegor08@gmail.com>
2026-01-23 20:14:33 +0300

    Working

--- Lab5true/Program.cs
+++ Lab5true/Program.cs
@@ -1,5 +1,37 @@
 ﻿using System;
-namespace Lab5
+namespace Lab5true
 {
-    class
+    class Program
+    {
+        static void Main()
+        {
+            int exit = 0;
+            Console.WriteLine("Здравствуйте, эта программма сделана для того, чтобы переводить введёные часы и минуты только в минуты, ведь вы сами вряд-ли сможете это сделать");
+            var time = new Time();
+
+            do
+            {
+                Console.WriteLine("Выберите действие: 0 - ввести данные, 1 - отобразить введённые данные, 2 - сосчитать время, 9 - выход из программы");
+                int input = int.Parse(Console.ReadLine());
+                switch (input)
+                {
+                    case 0:
+                        time.Read();
+                        break;
+                    case 1:
+                        time.Display();
+                        break;
+                    case 2:
+                        time.GetMinute();
+                        break;
+                    case 9:
+                        exit = 1;
+                        break;
+                    default:
+                        Console.WriteLine("Выберите от 0 до 2, либо 9!");
+                        break;
+                }
+            } while (exit == 0);
+        }
+    }
 }
--- Lab5true/Time.cs
+++ Lab5true/Time.cs
@@ -6,11 +6,30 @@ namespace Lab5true
 {
     internal class Time
     {
-        public int hour;
-        public int minute;
-        public void minutes()
+        private int hour = 0;
+        private int minute = 0;
+
+        public void GetMinute()
         {
-            minute = minute + hour * 60;
+            Console.WriteLine(hour * 60 + minute);
+        }
+
+        public void Read()
+        {
+            Console.WriteLine("Введите количество часов (от 0 до какого-то большого :) ): ");
+            hour = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите количество минут (от 0 до 59): ");
+            do
+            {
+                minute = int.Parse(Console.ReadLine());
+            } while (minute < 0 || minute > 59);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Часы: " +  hour);
+            Console.WriteLine("Минуты: " + minute);
         }
     }
 }
