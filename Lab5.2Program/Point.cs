efb2294c95d55e2555c811fdfc6d9a44d2ee772c
Егор Степанов <egorstepanovegor08@gmail.com>
2026-02-05 08:32:00 +0300

    2TaskLab

---
+++ Lab5.2Program/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab5._2Program
+{
+    internal class Point
+    {
+        private double xCoord;
+        private double yCoord;
+
+        public void xMove()
+        {
+            xCoord = xCoord + double.Parse(Console.ReadLine());
+        }
+
+        public void yMove()
+        {
+            yCoord = yCoord + double.Parse(Console.ReadLine());
+        }
+
+        public void getDistance()
+        {
+            Console.WriteLine(Math.Sqrt(xCoord * xCoord + yCoord * yCoord));
+        }
+    }
+}
---
+++ Lab5.2Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab5true
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
