863f59042344c4679fc0765a8c6573967cdb21b2
Егор Степанов <egorstepanovegor08@gmail.com>
2026-02-05 09:18:57 +0300

    2 working tasks

--- Lab5.2Program/Point.cs
+++ Lab5.2Program/Point.cs
@@ -4,24 +4,39 @@ using System.Text;
 
 namespace Lab5._2Program
 {
-    internal class Point
+    internal class PointA
     {
-        private double xCoord;
-        private double yCoord;
+        private double x;
+        private double y;
+
+        public PointA() : this(0.0, 0.0) { }
+
+        public PointA(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
 
         public void xMove()
         {
-            xCoord = xCoord + double.Parse(Console.ReadLine());
+            x = x + double.Parse(Console.ReadLine());
         }
 
         public void yMove()
         {
-            yCoord = yCoord + double.Parse(Console.ReadLine());
+            y = y + double.Parse(Console.ReadLine());
         }
 
         public void getDistance()
         {
-            Console.WriteLine(Math.Sqrt(xCoord * xCoord + yCoord * yCoord));
+            Console.WriteLine(Math.Sqrt(x * x + y * y));
+        }
+
+        public double DistantTo(PointA otherPoint)
+        {
+            double dx = this.x - otherPoint.x;
+            double dy = this.y - otherPoint.y;
+            return Math.Sqrt(dx * dx + dy * dy);
         }
     }
 }
--- Lab5.2Program/Program.cs
+++ Lab5.2Program/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Lab5._2Program;
+using System;
+using System.Drawing;
 
 namespace Lab5true
 {
@@ -6,7 +8,23 @@ namespace Lab5true
     {
         static void Main()
         {
+            Console.WriteLine("Здравствуйте, уважаемый пользователь. Это программа создана для работы с точками (обычно 1 или 2). Для начала, введите координаты для певрой точки (можно десятичные).");
+            PointA point1 = new PointA(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()));
 
+            Console.WriteLine("Укажите на сколько переместить первую точку по координате x");
+            point1.xMove();
+
+            Console.WriteLine("Укажите на сколько переместить первую точку по координате y");
+            point1.yMove();
+
+            Console.WriteLine("Сейчас программа выведет расстояние от первой точки до начала координат");
+            point1.getDistance();
+
+            Console.WriteLine("А теперь введите координаты второй точки");
+            PointA point2 = new PointA(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Расстояние между двумя точками равно: ");
+            Console.WriteLine(point1.DistantTo(point2));
         }
     }
 }
